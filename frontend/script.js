b543ecebc6bd1e99848cffad90a188108bbe636d
Jim Klick <jklick@ivytech.edu>
2025-04-11 10:39:10 -0400

    added http

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
 //eventlistener,triggers when DOM is loaded
 addEventListener("DOMContentLoaded", async function(){
-    const response = await this.fetch("localhost:300/api/songs");
+    const response = await this.fetch("http://localhost:300/api/songs");
 });
